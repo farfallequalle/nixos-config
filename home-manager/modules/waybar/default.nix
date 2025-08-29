4f1b0580ed4b875a76467c00111f7ed454b21ca2
farfallequalle <141916717+farfallequalle@users.noreply.github.com>
2025-08-29 15:55:06 +0200

    remove weather and language from waybar

--- home-manager/modules/waybar/default.nix
+++ home-manager/modules/waybar/default.nix
@@ -9,7 +9,7 @@
         height = 30;
         modules-left = ["hyprland/workspaces"];
         modules-center = ["hyprland/window"];
-        modules-right = ["hyprland/language" "custom/weather" "pulseaudio" "battery" "clock" "tray"];
+        modules-right = ["pulseaudio" "battery" "clock" "tray"];
         "hyprland/workspaces" = {
           disable-scroll = true;
           show-special = true;
@@ -20,7 +20,7 @@
             "1" = "";
             "2" = "";
             "3" = "";
-            "4" = "";
+            "4" = "";
             "5" = "";
             "6" = "";
             "7" = "";
@@ -32,13 +32,6 @@
           persistent-workspaces = {
             "*" = 9;
           };
-        };
-
-        "custom/weather" = {
-          format = " {} ";
-          exec = "curl -s 'wttr.in/Tashkent?format=%c%t'";
-          interval = 300;
-          class = "weather";
         };
 
         "pulseaudio" = {
