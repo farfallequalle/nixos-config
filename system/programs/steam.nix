dd7059d0f5b4231e8858bf83ed93598de50d9c2f
vaultgoblin64 <141916717+vaultgoblin64@users.noreply.github.com>
2026-02-02 15:57:14 +0100

    remove useless steam settings

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -1,5 +1,4 @@
 { pkgs, ... }:
-# to be verified
 {
   hardware.steam-hardware.enable = true;
   programs.steam = {
@@ -15,23 +14,9 @@
 
   # Add system packages for VR support
   environment.systemPackages = with pkgs; [
-    openvr # Required for SteamVR
-    libusb1 # Used for VR devices
     usbutils
     pkgs.libsndfile
     pkgs.xwayland
     gamescope
   ];
-
-  # Udev rules for VR devices
-  services.udev.packages = with pkgs; [
-    openvr
-  ];
-
-  services.udev.extraRules = ''
-    # HTC Vive
-    SUBSYSTEM=="usb", ATTR{idVendor}=="0bb4", ATTR{idProduct}=="2c87", MODE="0666", GROUP="plugdev"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="28de", ATTR{idProduct}=="2101", MODE="0666", GROUP="plugdev"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="28de", ATTR{idProduct}=="2000", MODE="0666", GROUP="plugdev"
-  '';
 }
