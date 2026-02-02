36fdcdcf9ff770729a6a44f114a9bc2aee9c189b
vaultgoblin64 <141916717+vaultgoblin64@users.noreply.github.com>
2026-02-02 23:50:21 +0100

    add swww wallpaper daemon

--- home/niri/autostart.nix
+++ home/niri/autostart.nix
@@ -1,7 +1,13 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, config, ... }:
 
+let
+  wallpaper = "${config.stylix.image}";
+  swww = "${pkgs.swww}/bin/swww";
+in
 {
   programs.niri.settings.spawn-at-startup = [
+    { command = [ swww "init" ]; }
+    { command = [ "sh" "-c" "sleep 1 && ${swww} img ${wallpaper}" ]; }
     { command = [ "vesktop" ]; }
     { command = [ "Telegram" ]; }
     { command = [ "zapzap" ]; }
